4c4ae6fa29fa6198aa5fae02dc89a63b7f4b0093
Murari1605 <gmurari35@gmail.com>
2025-01-28 12:53:10 +0530

    Add files via upload

--- clean data.py
+++ clean data.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[5]:
+# In[1]:
 
 
 #load the libraries
@@ -18,51 +18,51 @@ data = pd.read_csv("data_clean.csv")
 data
 
 
-# In[6]:
+# In[3]:
 
 
 data = pd.read_csv("data_clean.csv")
 print(data)
 
 
-# In[7]:
+# In[4]:
 
 
 data.info()
 
 
-# In[8]:
+# In[5]:
 
 
 print(type(data))
 print(data.shape)
 
 
-# In[9]:
+# In[6]:
 
 
 data.shape
 
 
-# In[10]:
+# In[7]:
 
 
 data.dtypes
 
 
-# In[ ]:
+# In[8]:
 
 
 # drop columns that are not needed
 
 
-# In[11]:
+# In[9]:
 
 
 data.describe()
 
 
-# In[14]:
+# In[10]:
 
 
 # drop dupplicate column and unnamed column
@@ -73,13 +73,13 @@ data1
 
 # 
 
-# In[16]:
+# In[11]:
 
 
 data1.info()
 
 
-# In[19]:
+# In[12]:
 
 
 # convert the month column data type to integer data type
@@ -88,7 +88,7 @@ data1['Month']=pd.to_numeric(data['Month'],errors='coerce')
 data1.info()
 
 
-# In[20]:
+# In[13]:
 
 
 # checking for duplicated rows in the table
@@ -96,14 +96,189 @@ data1.info()
 data1[data1.duplicated()]
 
 
-# In[21]:
+# In[14]:
 
 
 data1[data1.duplicated(keep = False)]
 
 
+# In[15]:
+
+
+data1.rename({'solar': 'solar'}, axis=1, inplace = True)
+data1
+
+
+# In[16]:
+
+
+data1.isnull().sum()
+
+
+# In[17]:
+
+
+cols = data1.columns
+colours = ['black', 'yellow']
+sns.heatmap(data1[cols].isnull(),cmap=sns.color_palette(colours),cbar = True)
+
+
+# In[18]:
+
+
+median_ozone = data1["Ozone"].median()
+mean_ozone = data1["Ozone"].mean()
+print("Median of Ozone: ", median_ozone)
+print("Mean of Ozone: ", mean_ozone)
+
+
+# In[19]:
+
+
+data1['Ozone'] = data1['Ozone'].fillna(median_ozone)
+data1.isnull().sum()
+
+
+# In[20]:
+
+
+median_Solar = data1["Solar"].median()
+mean_Solar = data1["Solar"].mean()
+print("Median of Solar: ", median_Solar)
+print("Mean of Ozone: ", mean_Solar)
+
+
 # In[ ]:
 
 
+data1['Solar.R'] = data1['Solar.R'].fillna(median_Solar.R)
+data1.isnull().sum()
 
+
+# In[ ]:
+
+
+print(data1["Weather"].value_counts())
+mode_weather = data1["Weather"].mode()[0]
+print(mode_weather)
+
+
+# In[ ]:
+
+
+data1["Weather"] = data1["Weather"].fillna(mode_weather)
+data1.isnull().sum()
+
+
+# In[ ]:
+
+
+data1["Month"] = data1["Month"].fillna(mode_weather)
+data1.isnull().sum()
+
+
+# In[ ]:
+
+
+data1.tail()
+
+
+# In[ ]:
+
+
+data1.reset_index(drop=True)
+
+
+# In[ ]:
+
+
+#Create a figure with two subplots ,stacked vertically
+fig, axes=plt.subplots(2, 1, figsize=(8, 6), gridspec_kw={'height_ratios':[1, 3]})
+#Plot the boxplot in the first (top) subplot
+sns.boxplot(data=data1["Ozone"], ax=axes[0],color='skyblue',width=0.5, orient='h')
+axes[0].set_title("Boxplot")
+axes[0].set_xlabel("Ozone Levels")
+
+#Plot the histogram with KDE Curve in the second (bottom) subplot
+sns.histplot(data1["Ozone"], kde=True, ax=axes[1],color='purple',bins=30)
+axes[1].set_title("Histogram with KDE")
+axes[1].set_xlabel("Ozone Levels")
+axes[1].set_ylabel("Frequency")
+ 
+#Adjust layout for better spacing
+plt.tight_layout()
+
+#Show the plot
+plt.show()
+
+
+
+
+# # observations
+# .the ozone colun has extreme values beyond 81 as seen from box plot
+# .the same is confirmed from the below right-skewed histogram
+
+# In[ ]:
+
+
+#Create a figure with two subplots ,stacked vertically
+fig, axes=plt.subplots(2, 1, figsize=(8, 6), gridspec_kw={'height_ratios':[1, 3]})
+#Plot the boxplot in the first (top) subplot
+sns.boxplot(data=data1["Solar"], ax=axes[0],color='skyblue',width=0.5, orient='h')
+axes[0].set_title("Boxplot")
+axes[0].set_xlabel("Solar Levels")
+
+#Plot the histogram with KDE Curve in the second (bottom) subplot
+sns.histplot(data1["Solar"], kde=True, ax=axes[1],color='purple',bins=30)
+axes[1].set_title("Histogram with KDE")
+axes[1].set_xlabel("solar Levels")
+axes[1].set_ylabel("Frequency")
+ 
+#Adjust layout for better spacing
+plt.tight_layout()
+
+#Show the plot
+plt.show()
+
+
+# In[21]:
+
+
+plt.figure(figsize=(6,2))
+plt.boxplot(data1["Ozone"], vert=False)
+
+
+# In[22]:
+
+
+plt.figure(figsize=(6,2))
+boxplot_data = plt.boxplot(data1["Ozone"], vert=False)
+[item.get_xdata() for item in boxplot_data['fliers']]
+
+
+# In[23]:
+
+
+# Method 2 for outlier detection
+data1["Ozone"].describe()
+
+
+# In[32]:
+
+
+mu = data1["Ozone"].describe()[1]
+sigma = data1["Ozone"].describe()[2]
+for x in data1["Ozone"]:
+    if ((x < (mu -3*sigma)) or (x > (mu + 3*sigma))): 
+        print(x)
+
+
+# In[29]:
+
+
+import scipy.stats as stats
+plt.figure(figsize=(8, 6))
+stats.probplot(data1["Ozone"], dist="norm", plot=plt)
+plt.title("Q-Q plot for Outlier Detection", fontsize=14)
+plt.xlabel("Theoretical Quantiles", fontsize=12)
 
